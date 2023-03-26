2f74306d07abd364ac2d4e4c932cba90ede289c7
Aman <amanb2704@gmail.com>
2023-03-26 21:26:20 +0530

    redis added

--- server.js
+++ server.js
@@ -1,42 +1,95 @@
-const WebSocket = require('ws');
-const Y = require("yjs"); 
-
-const wss = new WebSocket.Server({ port: 8080 });
+const WebSocket = require("ws");
+const Y = require("yjs");
+const redis = require("redis");
+const { commandOptions } = require("redis");
 
 const sockets = [];
-const yDoc = new Y.Doc(); 
-const yArray = yDoc.getArray("editor-1"); 
 
-wss.on('connection', function connection(ws) {
+/* Redis Setup */
+const client = redis.createClient({});
+
+(async () => {
+  await client.connect();
+})();
+
+client.on("error", (err) => {
+  console.log("Error " + err);
+});
+
+client.on("ready", () => {
+  console.log("Connected to redis");
+});
+
+// (async () => {
+//   const uint8Array = Y.encodeStateAsUpdate(yDoc);
+//   const buffer = Buffer.from(uint8Array.buffer);
+//   await client.hSet("foo", "field", buffer);
+//   const value = await client.hGetAll(
+//     commandOptions({ returnBuffers: true }),
+//     "foo"
+//   );
+
+// })();
+
+/* Websocket Setup */
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on("connection", async function connection(ws) {
   sockets.push(ws);
 
-  const uint8Array = Y.encodeStateAsUpdate(yDoc); 
-  console.log(Buffer.from(uint8Array.buffer));
-  ws.send(JSON.stringify({type:"init", uint8Array:Buffer.from(uint8Array.buffer)})); 
+  const docExists = await client.exists("document");
+  if (docExists) {
+    const docBuffer = await client.hGetAll(
+      commandOptions({ returnBuffers: true }),
+      "document"
+    );
+    ws.send(JSON.stringify({ type: "init", uint8Array: docBuffer.field }));
+  } else {
+    const yDoc = new Y.Doc();
+    const uint8Array = Y.encodeStateAsUpdate(yDoc);
+    const docBuffer = Buffer.from(uint8Array.buffer);
+    ws.send(JSON.stringify({ type: "init", uint8Array: docBuffer }));
+    await client.hSet("document", "field", docBuffer);
+  }
 
-  console.log(`New socket connected. Total number of sockets: ${sockets.length}`);
-  ws.on('message', function incoming(message) {
+  console.log(
+    `New socket connected. Total number of sockets: ${sockets.length}`
+  );
+
+  ws.on("message", async function incoming(message) {
     console.log(`Received message:`, message);
 
+    /*DocBuffer setup*/
+    const docBuffer = await client.hGetAll(
+      commandOptions({ returnBuffers: true }),
+      "document"
+    );
 
-    Y.applyUpdate(yDoc, message); 
+    const yDoc = new Y.Doc();
+    Y.applyUpdate(yDoc, docBuffer.field);
+    Y.applyUpdate(yDoc, message);
+
+    const uint8Array = Y.encodeStateAsUpdate(yDoc);
+    const newDocBuffer = Buffer.from(uint8Array.buffer);
+    console.log("docBuffer: ", docBuffer);
+    console.log("newDocBuffer: ", newDocBuffer);
+    await client.hSet("document", "field", newDocBuffer);
+
     // Forward message to all other sockets
     sockets.forEach(function (socket) {
       if (socket !== ws && socket.readyState === WebSocket.OPEN) {
-        const data = {type:"update", uint8Array:message}; 
-        socket.send(JSON.stringify(data)); 
+        const data = { type: "update", uint8Array: message };
+        socket.send(JSON.stringify(data));
       }
     });
   });
 
-  ws.on('close', function () {
+  ws.on("close", function () {
     const index = sockets.indexOf(ws);
     if (index !== -1) {
       sockets.splice(index, 1);
-      console.log(`Socket disconnected. Total number of sockets: ${sockets.length}`);
+      console.log(
+        `Socket disconnected. Total number of sockets: ${sockets.length}`
+      );
     }
   });
-
-
-  
 });
